5d2bd552c74b69acbb086e2873a81d6da9fc39f3
ediswt12 <@edisim7276@gmail.com>
2025-11-22 16:51:16 +0400

    part 6

--- djangoproject/polls/views.py
+++ djangoproject/polls/views.py
@@ -11,6 +11,8 @@ from .models import Choice, Question
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 def get_queryset(self):
     """
